6558c1c5aac9f31853091f2113d984126552b39f
ryokusakari <ryotaro119.k@gmail.com>
2026-07-01 12:10:24 +0100

    Add portfolio styling and animations

--- workshop/static/js/main.js
+++ workshop/static/js/main.js
@@ -1,9 +1,30 @@
+// control video opactiy with scroll
+const videoBackground = document.querySelector(".video-background");
+
+function updateVideoOpacity() {
+    const fadeDistance = window.innerHeight*2;
+    const scrollProgress = window.scrollY / fadeDistance;
+    const limitedProgress = Math.min(Math.max(scrollProgress, 0.4), 1);
+
+    videoBackground.style.opacity = 1 - limitedProgress;
+}
+
+window.addEventListener("scroll", updateVideoOpacity, {
+    passive: true,
+});
+
+window.addEventListener("resize", updateVideoOpacity);
+
+updateVideoOpacity();
+
+// select form components
 const contactForm = document.querySelector("form");
 const contactName = document.querySelector("#contact-name");
 const contactEmail = document.querySelector("#contact-email");
 const contactMessage = document.querySelector("#contact-message");
 const contactResponse = document.querySelector("#contact-response");
 
+// listen to submission event
 contactForm.addEventListener("submit", async function (event) {
     event.preventDefault();
     const name = contactName.value.trim();
